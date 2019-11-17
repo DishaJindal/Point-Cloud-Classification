724d6b552604faf9de4feec60eebb2f2397cac07
DISHA JINDAL <djjindal@park.key.seas.upenn.edu>
2019-11-16 22:01:54 -0500

    some comments

--- point_cloud_classification/graph/graph.h
+++ point_cloud_classification/graph/graph.h
@@ -1,4 +1,4 @@
-#include <cuda.h>
+﻿#include <cuda.h>
 #include <cuda_runtime.h>
 #include "common.h"
 #include "device_launch_parameters.h"
@@ -14,9 +14,26 @@ namespace Graph {
 			float* get_A();
 			float* get_Lnorm();
 		private:
+			/* 
+			  Exponent of L2 distance between all nodes
+			  Sorts by distance
+			  Keeps k nearest
+			*/
 			float* find_adj(float *points, int n, int f_in, int k);
+			
+			/*
+				I − D^(−1/2) * W * (D^(−1/2))
+			*/
 			float* normalize_adj(const float* A);
+			
+			/*
+				I - Lnorm
+			*/
 			float* find_laplacian(const float* Anorm);
+			
+			/* 
+				Lnorm = 2L/λmax − I
+			*/
 			float* normalize_laplacian(const float* L, const int n);
 	};
 }
