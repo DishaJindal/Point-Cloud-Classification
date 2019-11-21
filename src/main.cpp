5387ce09c591ef15123676d8fcb5cc7e230ebd07
DISHA JINDAL <djjindal@park.key.seas.upenn.edu>
2019-11-21 16:01:05 -0500

    test func

--- src/main.cpp
+++ src/main.cpp
@@ -29,42 +29,6 @@
 using namespace std;
 using namespace PointCloudClassification;
 
-int main(int argc, char* argv[]) {
-
-	// Read data from file and store it as a vector of float pointers (length of vector -> number of samples | each sample -> 1024 x 3 floats)
-	
-	//vector<float*> samples; //Data from file will be stored here
-
-	// Construct graph for each example and store a vector of L (Laplacians) and AX for each sample
-	vector<float*> laplacians;
-	vector<float*> transformed_inputs;
-	//int l1_features = 3;
-
-	/*for (int i = 0; i < PointCloudClassification::num_points; i++) {
-		float* current_sample = samples[i];
-		Graph::Graph g (current_sample, num_points, l1_features, num_neighbours);
-
-		float* A = g.get_A();
-		MatrixCPU* m = new MatrixCPU();
-		float* AX = (float*)malloc(num_points * l1_features * sizeof(float));
-		m->multiply(A, current_sample, num_points, num_points, l1_features, AX);
-		transformed_inputs.push_back(AX);
-
-		float* L = g.get_Lnorm();
-		laplacians.push_back(L);
-	}*/
-
-	// Tests
-	tests();
-	
-	//Build the network
-	
-
-	// Train 
-	int number_of_batches = ceil(Parameters::num_points / Parameters::batch_size);
-
-}
-
 void tests() {
 	cout << "********************************************************" << endl;
 	cout << "Testing Matrix Transpose ..." << endl;
@@ -111,3 +75,40 @@ void tests() {
 	Tests::testAllBackward();
 	cout << "********************************************************" << endl;
 }
+
+int main(int argc, char* argv[]) {
+
+	// Read data from file and store it as a vector of float pointers (length of vector -> number of samples | each sample -> 1024 x 3 floats)
+	
+	//vector<float*> samples; //Data from file will be stored here
+
+	// Construct graph for each example and store a vector of L (Laplacians) and AX for each sample
+	vector<float*> laplacians;
+	vector<float*> transformed_inputs;
+	//int l1_features = 3;
+
+	/*for (int i = 0; i < PointCloudClassification::num_points; i++) {
+		float* current_sample = samples[i];
+		Graph::Graph g (current_sample, num_points, l1_features, num_neighbours);
+
+		float* A = g.get_A();
+		MatrixCPU* m = new MatrixCPU();
+		float* AX = (float*)malloc(num_points * l1_features * sizeof(float));
+		m->multiply(A, current_sample, num_points, num_points, l1_features, AX);
+		transformed_inputs.push_back(AX);
+
+		float* L = g.get_Lnorm();
+		laplacians.push_back(L);
+	}*/
+
+	// Tests
+	tests();
+	
+	//Build the network
+	
+
+	// Train 
+	int number_of_batches = ceil(Parameters::num_points / Parameters::batch_size);
+}
+
+
